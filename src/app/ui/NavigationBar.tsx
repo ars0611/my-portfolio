d01f502da3f2c5925eec570d57ba0e8f030eaf19
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-09-22 23:49:38 +0900

    fix: Navigation bar

--- src/app/ui/NavigationBar.tsx
+++ src/app/ui/NavigationBar.tsx
@@ -13,20 +13,20 @@ const pages = [
 ];
 
 export function NavigationBar() {
-    const pathname = usePathname();
+    const pathname = usePathname(); // 現在のパスを取得
     return(
-            <nav className="fixed top-0 left-0 w-full z-50 p-4">
-                <div className = "max-w-7xl mx-auto text-white text-xl flex items-center justify-center gap-2 bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg px-2">
-                    <a href="https://github.com/ars0611" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:bg-green-400/40 rounded-md px-4 py-2">
+            <nav className="fixed top-0 left-0 w-full z-50 p-8"> {/* navigation bar を画面上部に固定 */}
+                <div className = "text-white text-xl flex justify-between bg-gray-200/30 backdrop-blur-lg rounded-md border border-[#D4CEFF] shadow-lg"> {/* 背景を半透明にしてぼかし効果を追加 */}
+                    <a href="https://github.com/ars0611" target="_blank" rel="noopener noreferrer" className="flex items-left gap-2 hover:bg-green-400/40 rounded-md px-4 py-2">
                         <span className="text-[1.5rem]"><VscGithubInverted /></span>
-                        <span className="hidden md:block">GitHub</span>
+                        <span className="hidden md:block">GitHub</span> {/* md以上の画面サイズで表示 i.e. 画面が十分小さい場合アイコンのみ表示 */}
                     </a>
                     {pages.map((page) => {
                         return(
-                            <div className ="px-8" key={page.name}>
+                            <div className ="flex items-center" key={page.name}>
                                 <Link
                                     href={page.path}
-                                    className={`flex items-center gap-2 hover:bg-green-400/40 rounded-md px-4 py-2 ${pathname === page.path ? "bg-green-400/40" : ""}`}
+                                    className={`flex items-center hover:bg-green-400/40 rounded-md px-4 py-2 ${pathname === page.path ? "bg-green-400/40" : ""}`} // 現在のページに対応するリンクの背景色を変更
                                 >
                                     <span className="text-[1.5rem]">{page.icon}</span>
                                     <span className="hidden md:block">{page.name}</span>
@@ -36,43 +36,8 @@ export function NavigationBar() {
                     })}
                 </div>
             </nav>
-    );}
-
-
-    {/* const pathname = usePathname();
-    return(
-            <nav className="p-4 m-4 flex justify-center fixed top-0 left-0 z-10">
-                <div className = "text-white text-xl flex justify-center bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg">
-                    {pages.map((page) => {
-                        const isExternal = page.path.startsWith("http");
-                        return (
-                            <div className ="m-4" key={page.name}>
-                            {isExternal ? (
-                                    <a
-                                        href={page.path}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="flex items-center hover:bg-green-400/40 rounded-md p-1"
-                                    >
-                                        <span className="text-[1.5rem]">{page.icon}</span>
-                                        <span className="hidden md:block">{page.name}</span>
-                                    </a>
-                                ) : (
-                                    <Link
-                                        href={page.path}
-                                        className={`flex items-center hover:bg-green-400/40 rounded-md p-1 px-4 ${pathname === page.path ? "bg-green-400/40" : ""}`}
-                                    >
-                                        <span className="text-[1.5rem]">{page.icon}</span>
-                                        <span className="hidden md:block">{page.name}</span>
-                                    </Link>
-                                )}
-                            </div>
-                        );
-                    })}
-                </div>
-            </nav>
-    );*/}
-
+    );
+}
 
 // Ref: https://react-icons.github.io/react-icons/
 // Ref: https://qiita.com/ruiiixiii/items/37fa4a6e3a7745b2437d
